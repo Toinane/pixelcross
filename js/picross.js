7269ba57e4fc09a74ea8d6f7a8d725296e67c33c
Toinane <toinane@crea-that.fr>
2018-12-20 15:32:42 +0100

    can random generate level

--- js/picross.js
+++ js/picross.js
@@ -1,131 +1,93 @@
+async function createLevel(level) {
+   let game = '<thead><td></td>';
 
+   await level.clues.vertical.map(async (line, i) => {
+      game += '<td>';
+      await line.map(number => game += `<span>${number}</span>`);
+      game += '</td>';
+   });
 
-function Picross(){
+   game += '</thead><tbody>';
 
+   console.log(game)
 
-   /*
-      CREATELEVEL
-    */
-   /*this.createLevel = function(levelpack, number){
-      let level = levelpack.levels[number],
-            content_tab = '<tr><td id="0-0" class="blank"></td>',
-            x = 1, y = 1, tempY = {};
+   await level.map.map(async (line, h) => {
+      game += `<tr><th>`;
+      level.clues.horizontal[h].map(number => game += `<span>${number}</span>`);
+      game += '</th>'
+      line.map((pixel, w) => {
+         const td = document.createElement('td');
+         if(h % 5 === 0 && h !== 0 && h !== level.map.length) td.classList.add('h-separator');
+         if(w % 5 === 0 && w !== 0 && w !== line.length) td.classList.add('w-separator');
+         if(pixel) td.classList.add('black');
+         td.id = `pos-${h}-${w}`;
 
-      for(let i = 0, len = level.level; i < len; i++){
-         let char = level.level.charAt(i);
-         if(char === "#"){
-            content_tab += '<td id="'+x+'-'+y+'"></td>';
-            tempY[y] =
-            x++;
-         }
-         if(char === "-"){
-            content_tab += '<td id="'+x+'-'+y+'"></td>';
-            x++;
-         }
-         if(char === ":"){
-            content_tab += '</tr><tr><td id="0-'+y+'" class="blank"></td>';
-            y++;
-            x = 1;
-         }
-      }
-      content_tab += '</tr>';
-      document.querySelector('.picross tbody').innerHTML = content_tab;
-   }*/
+         game += td.outerHTML;
+      });
+      game += '</tr>';
+   });
 
-   this.createLevel = function(levelpack, number){
-      let level = levelpack.levels[number], size = {x: 0, y: 0}, numY = {}, numX = {};
-      console.log(level.level);
-      for(el in level.level){
-         console.log(el);
-      }
-   }
-
-
-   /*
-      STARTEVENTS
-    */
-   this.startEvents = function(){
-      let isFirstDown = false, isSecondDown = false, isHighlighted,
-      remote = require('electron').remote;
-
-      let els = document.querySelectorAll('.picross p');
-      for(el of els){
-         el.onmousedown = function(e){
-            if(e.button == 0){
-               this.classList.toggle('green');
-            }
-            return false;
-         }
-      }
-
-      els = document.querySelectorAll('.picross tbody td:not(.blank)');
-      for(el of els){
-         el.onmousedown = function(e){
-            if(e.button == 0){
-               isFirstDown = true;
-               this.classList.toggle('black');
-               if(this.classList.contains('check')){this.classList.toggle('check');}
-               isHighlighted = this.classList.contains("black");
-               return false;
-            }
-            else if(e.button == 2){
-               isSecondDown = true;
-               this.classList.toggle('check');
-               if(this.classList.contains('black')){this.classList.toggle('black');}
-               isHighlighted = this.classList.contains("check");
-               return false;
-            }
-         }
-         el.onmouseover = function(){
-            if(isFirstDown){
-               if(this.classList.contains('check')){this.classList.toggle('check');}
-               this.classList.toggle("black", isHighlighted);
-            }
-            else if(isSecondDown){
-               if(this.classList.contains('black')){this.classList.toggle('black');}
-               this.classList.toggle("check", isHighlighted);
-            }
-         }
-         el.onselect = function(){
-           return false;
-         }
-      }
-
-      document.onmouseup = function(){
-         isFirstDown = false;
-         isSecondDown = false;
-      }
-
-
-      document.querySelector('#minimize').onclick = function(){
-         remote.getCurrentWindow().minimize();
-      }
-      document.querySelector('#square').onclick = function(){
-         var window = remote.getCurrentWindow();
-         if(this.maximize){
-            window.unmaximize();
-            this.maximize = false;
-         }
-         else{
-            window.maximize();
-            this.maximize = true;
-         }
-      }
-      document.querySelector('#close').onclick = function(){
-         remote.getCurrentWindow().close();
-      }
-   }
-
-
-   /*
-      STARTGAME
-    */
-   this.startGame = function(){
-      let = levelpack = require(__dirname+'/js/levels/levelpack.json');
-      this.createLevel(levelpack, 1);
-      this.startEvents();
-   }
-   this.startGame();
+   document.getElementById('game').innerHTML = game + '</tbody>';
 }
 
-var Picross = new Picross();
+function randomizerMap(width, height) {
+   let map = [];
+   for(let h = 0; h < height; h++) {
+      map[h] = [];
+      for(let w = 0; w < width; w++) {
+         const input = Math.round(Math.random());
+         map[h].push(input); 
+      }
+   }
+
+   return map;
+}
+
+function generateClues(map) {
+   let horizontal = [];
+   let vertical = [];
+
+   map.map((line, i) => {
+      horizontal[i] = [];
+      let serie = 0;
+      line.map(pixel => {
+         if(pixel) serie++;
+         else {
+            if(serie) horizontal[i].push(serie);
+            serie = 0;
+         }
+      });
+      if(serie) horizontal[i].push(serie);
+   });
+
+   for(let w = 0; w < map[0].length; w++) {
+      vertical[w] = [];
+      let serie = 0;
+      for(let h = 0; h < map.length; h++) {
+         if(map[h][w]) serie++;
+         else {
+            if(serie) vertical[w].push(serie);
+            serie = 0;
+         }
+      }
+      if(serie) vertical[w].push(serie);
+   }
+
+   return {
+      map: map,
+      clues: {
+         horizontal: horizontal,
+         vertical: vertical
+      }
+   }
+}
+
+function main() {
+   const map = randomizerMap(4, 4);
+   const level = generateClues(map);
+
+   createLevel(level);
+   console.log(level)
+}
+
+main();
